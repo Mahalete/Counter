1a96f9c0044e741a696706e117e9dac294744445
BC-macair011 <mahialemu@gmail.com>
2021-10-26 06:56:46 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <small> Made by Mahalete Haile</small>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+const Main = () => {
+  return (
+    <main>
+      <div class="numberCircle">
+        <h1>0</h1>
+      </div>
+      <div class="btn-wrapper">
+        <button class="addBtn">Add one</button>
+        <button class="removeBtn">Remove one</button>
+        <button class="resetBtn">Reset</button>
+      </div>
+    </main>
+  );
+};
+export default Main;
